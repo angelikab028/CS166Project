7e3befd2a78ed00dbced18277b86e239bcd7d8f8
Yuchen Zhu <yuchenzhu@Yuchens-MacBook.local>
2023-03-21 13:49:40 -0700

    Added viewHotels() sql in queries.sql

--- sql/src/queries.sql
+++ sql/src/queries.sql
@@ -1,1 +1,33 @@
-test test
+--public static void viewHotels(Hotel esql) {}
+SELECT h.hotelName
+FROM Hotel h,
+WHERE calculate_distance(h.latitude, h.longitude,
+                         -- user inputs)
+                         < 30.0;
+
+--public static void viewRooms(Hotel esql) {}
+
+
+--public static void bookRooms(Hotel esql) {}
+
+
+--public static void viewRecentBookingsfromCustomer(Hotel esql) {}
+
+
+--public static void updateRoomInfo(Hotel esql) {}
+
+
+--public static void viewRecentUpdates(Hotel esql) {}
+
+
+--public static void viewBookingHistoryofHotel(Hotel esql) {}
+
+
+--public static void viewRegularCustomers(Hotel esql) {}
+
+
+--public static void placeRoomRepairRequests(Hotel esql) {}
+
+
+--public static void viewRoomRepairHistory(Hotel esql) {}
+
